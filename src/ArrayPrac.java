744087c276c828e5279f03b6fc74ec3d5ac6f168
muhsaif <muh.saif2001@gmail.com>
2026-06-22 19:54:27 +0500

    Pattern3 and 4 and 5

--- src/ArrayPrac.java
+++ src/ArrayPrac.java
@@ -4,10 +4,6 @@ public class ArrayPrac {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        // int[] arr = {2,4,5,6,8,9};
-        // for (int num : arr){
-        //     System.out.print(num + " ");
-        // }
         int[] arr = new int[n];
         for(int i = 0; i<n; i++){
             arr[i] = sc.nextInt();
